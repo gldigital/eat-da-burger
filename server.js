b0b15bd2d20fa284961525d48a253b31e22f0e9c
Greg Ladden <greg@gl-digital.com>
2017-11-06 21:06:12 -0600

    burger add and delete functionality

--- server.js
+++ server.js
@@ -1,4 +1,35 @@
-var express = require('express');
-var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
 
+// Import routes and give the server access to them.
+var burgers_Controller = require("./controllers/burgers_Controller.js");
+var burger = require("./models/burger.js");
+
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", function(req, res) {
+    burger.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+
+app.use("/api/burgers", burgers_Controller);
+
+app.listen(port);
